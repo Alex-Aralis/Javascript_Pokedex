8fe22386d18a74f48d62494fda422dd47dde2615
Josh <jogould@umail.iu.edu>
2016-06-09 14:08:29 -0400

    append pokemon names to list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 $(document).foundation()
+
+var Pokedex = {
+
+    template: template = $("#li_template")
+            .detach()
+            .removeAttr("id"),
+
+    addPokemonToList: function(pokemon) {
+        var li = this.template.clone().bind(this);
+        var list = $("#pokemon_list");
+        li.text(pokemon.pokemon_species.name);
+        list.append(li);
+    },
+
+    loadPokemon: function(pokemon) {
+        $.each(pokemon.pokemon_entries, function(i, pokemon) {
+            this.addPokemonToList(pokemon);
+        }.bind(this));
+    },
+
+    getAllPokemon: function() {
+        $.get({
+            url: this.url,
+        }).done(this.loadPokemon.bind(this));
+    },
+
+    init: function(url) {
+        this.url = url;
+        if(!/^.*\/$/.test(url))
+            this.url += "/";
+
+        this.getAllPokemon()
+    },
+};
+
+Pokedex.init("https://pokeapi.co/api/v2/pokedex/2");
