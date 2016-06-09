0b3346f0c7c9c7962375a665fb1e8d8a1f20ec78
Josh <jogould@umail.iu.edu>
2016-06-09 16:37:03 -0400

    load images with the pokemon names

--- js/app.js
+++ js/app.js
@@ -9,7 +9,10 @@ var Pokedex = {
     addPokemonToList: function(pokemon) {
         var li = this.template.clone().bind(this);
         var list = $("#pokemon_list");
+        var icon = li.find(".pokemon-icon");
+        icon.attr("src", "http://pokeapi.co/media/sprites/pokemon/" + pokemon.entry_number + ".png");
         li.text(pokemon.pokemon_species.name);
+        li.append(icon);
         list.append(li);
     },
 
